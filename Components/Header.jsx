b896b223d7a2980e9272ca76fa7e9fc33b3288f4
Harsh Telang <telangharsh06@gmail.com>
2023-11-08 20:10:56 +0530

    Login and Signup pages created
    TODO: backend and DB config

--- Components/Header.jsx
+++ Components/Header.jsx
@@ -16,7 +16,7 @@ const Header = () => {
             <li className='flex flex-col items-center justify-center gap-1 cursor-pointer ' onClick={()=>{setmenu("kids")}}><Link category="kid" href="/Kids" banner={"/Assets/banner_kids.png"}>Kids{menu==="kids"? <hr className='border-none w-11/12 h-1 rounded bg-red-600'/>:<></>}</Link></li>
         </ul> 
         <div id="logincart" className='flex items-center gap-10'>
-            <Link href="/LogIn"><button className='w-32 h-14 outline-none border-gray-300 border  rounded-full text-lg cursor-pointer text-black'>Login</button></Link>
+            <Link href="/Login"><button className='w-32 h-14 outline-none border-gray-300 border  rounded-full text-lg cursor-pointer text-black'>Login</button></Link>
             <Link href="/cart"><img src="/Assets/cart_icon.png" alt="" /></Link>
             <div id="cartcount" className='w-4 h-4 bg-red-500 flex justify-center items-center -mt-8 -ml-11 rounded-full text-sm text-white'>0</div>
         </div>
--- app/Signup/page.jsx
+++ app/Signup/page.jsx
@@ -1,12 +1,11 @@
-'use client';
-import React from "react";
-import Link from "next/link";
+import React from 'react';
+import Link from 'next/link';
 
-const login = () => {
+const signup = () => {
   return (
     <div id="login" className=' flex w-screen h-screen pt-10 bg-gradient-to-b from-gray-400 to-white items-center justify-center'>
       <div id="container" className='w-3/5 h-4/5 bg-white shadow-2xl'>
-        <h1 className=' mx-5 text-5xl font-medium mt-6'>Login</h1>
+        <h1 className=' mx-5 text-5xl font-medium mt-6'>Signup</h1>
         <div id="signUpField" className='flex items-center flex-col pap-5 mt-5'>
           <input className='w-4/5 h-12 rounded-xl  border border-gray-400 outline-none text-gray-500 text-lg  m-3 pl-20' type="text" placeholder='Your name'/>
           <input className='w-4/5 h-12 rounded-xl  border border-gray-400 outline-none text-gray-500 text-lg pl-20 m-3 ' type="email" placeholder='Email Address'/>
@@ -14,8 +13,8 @@ const login = () => {
         </div>
         <button className='w-4/5 h-12 bg-red-600 text-white rounded-xl flex items-center justify-center mt-6 mx-16'>Continue</button>
         
-            <Link href="/Signup">Visit Signup Page</Link>
-        <p id='login?' className='text-gray-400 w-4/5 mx-44 mt-10'>Don't have a account ?
+            <Link href="/Login">Visit Login Page</Link>
+        <p id='login?' className='text-gray-400 w-4/5 mx-44 mt-10'>Already have an account ?
            </p>
         <div id="agree" className='flex mx-36 w-4/5'>
           <input type="checkbox" name="" id="" className='mx-2'/>
@@ -27,4 +26,4 @@ const login = () => {
   )
 }
 
-export default login
+export default signup
